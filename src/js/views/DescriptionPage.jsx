39f616804b3c789da831e13316304cdb9d0dd7d7
Federico Serron <fede20041@gmail.com>
2025-03-22 10:39:17 +1030

    Favorites navigation fixed

--- src/js/views/DescriptionPage.jsx
+++ src/js/views/DescriptionPage.jsx
@@ -1,17 +1,12 @@
 import React, { useState, useEffect, useContext } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { Spinner } from "../component/Spinner.jsx";
-
 import { Context } from "../store/appContext";
 
-
 export const DescriptionPage = () => {
-
     const { store, actions } = useContext(Context);
-    const location = useLocation();
-    const params = useParams();
+    const { type, id } = useParams(); // Aquí obtenemos tanto el type como el id
 
-    const [type, setType] = useState(location.pathname.split('/')[1]);
     const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
@@ -20,24 +15,23 @@ export const DescriptionPage = () => {
                 try {
                     await actions.getCurrentItem(type, id);
                 } catch (error) {
-                    console.error(`There was an error trying to get the ${type} item`, error)
+                    console.error(`There was an error trying to get the ${type} item`, error);
                 }
             }
         }
 
         setIsLoading(true);
         if (!store.currentItem.length > 0) {
-            fetchCurrentItem(type, params.id).then(() => setIsLoading(false))
+            fetchCurrentItem(type, id).then(() => setIsLoading(false));
         } else {
             setIsLoading(false);
         }
 
         return () => {
-            actions.cleanUpCurrentItem()
-        }
-
-    }, [type, params.id]);
-
+            console.log("Se desmontó el componente");
+            actions.cleanUpCurrentItem();
+        };
+    }, [type, id, store.flagStore]);
 
     useEffect(() => {
         window.scrollTo(0, 0);
@@ -52,7 +46,9 @@ export const DescriptionPage = () => {
                     <div className="card-body">
                         <div className="row">
                             <div className="col-lg-6 col-md-5 col-sm-6">
-                                <div className="white-box text-center"><img src={store.images[type]} className="img-fluid" /></div>
+                                <div className="white-box text-center">
+                                    <img src={store.images[type]} className="img-fluid" />
+                                </div>
                             </div>
                             <div className="col-lg-6 col-md-7 col-sm-6">
                                 <h3 className="card-title">{store.currentItem?.properties?.name}</h3>
@@ -73,7 +69,7 @@ export const DescriptionPage = () => {
                                                     <td>{store.currentItem.properties?.gender}</td>
                                                 </tr>
                                                 <tr>
-                                                    <td>Sking Color</td>
+                                                    <td>Skin Color</td>
                                                     <td>{store.currentItem.properties?.skin_color}</td>
                                                 </tr>
                                                 <tr>
@@ -176,7 +172,6 @@ export const DescriptionPage = () => {
                                                 </tr>
                                             </tbody>
                                         )}
-
                                     </table>
                                 </div>
                             </div>
@@ -184,7 +179,6 @@ export const DescriptionPage = () => {
                     </div>
                 </div>
             )}
-
         </div>
-    )
-}
+    );
+};
